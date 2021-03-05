d4cc57a39905bd2ad419ae23fdeebeb920d8b16a
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2021-03-05 18:01:24 +0000

    fix

--- admin/pt/index.js
+++ admin/pt/index.js
@@ -1,16 +1,16 @@
-import alerts from "./alerts.json";
-import auctions from "./auctions.json";
-import buttons from "./buttons.json";
-import companies from "./companies.json";
-import countries from "./countries.json";
-import crowdfundings from "./crowdfundings.json";
-import forms from "./forms.json";
-import institutions from "./institutions.json";
-import leftMenu from "./leftMenu.json";
-import login from "./login.json";
-import main from "./main.json";
-import requests from "./requests.json";
-import tickets from "./tickets.json";
+const alerts = require("./alerts.json");
+const auctions = require("./auctions.json");
+const buttons = require("./buttons.json");
+const companies = require("./companies.json");
+const countries = require("./countries.json");
+const crowdfundings = require("./crowdfundings.json");
+const forms = require("./forms.json");
+const institutions = require("./institutions.json");
+const leftMenu = require("./leftMenu.json");
+const login = require("./login.json");
+const main = require("./main.json");
+const requests = require("./requests.json");
+const tickets = require("./tickets.json");
 
 const translation = Object.assign(
   alerts,
@@ -28,4 +28,4 @@ const translation = Object.assign(
   tickets,
 );
 
-export default translation;
+module.exports = translation;
