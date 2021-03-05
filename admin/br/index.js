79a3b504776d12002963086a97158e9e85d1efde
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2021-03-05 15:23:55 +0000

    added all the translations from admin project

---
+++ admin/br/index.js
@@ -0,0 +1,31 @@
+import alerts from "./alerts.json";
+import auctions from "./auctions.json";
+import buttons from "./buttons.json";
+import companies from "./companies.json";
+import countries from "./countries.json";
+import crowdfundings from "./crowdfundings.json";
+import forms from "./forms.json";
+import institutions from "./institutions.json";
+import leftMenu from "./leftMenu.json";
+import login from "./login.json";
+import main from "./main.json";
+import requests from "./requests.json";
+import tickets from "./tickets.json";
+
+const translation = Object.assign(
+  alerts,
+  auctions,
+  buttons,
+  companies,
+  countries,
+  crowdfundings,
+  forms,
+  institutions,
+  leftMenu,
+  login,
+  main,
+  requests,
+  tickets,
+);
+
+export default translation;
