43cda695df3ef8dcd9e0d54984e4608d894d311c
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2021-03-30 12:55:30 +0100

    added tests and missing translations

--- __tests__/projects/whitelabel.test.js
+++ __tests__/projects/whitelabel.test.js
@@ -1,25 +1,49 @@
+const project = "whitelabel";
+
 const translations = {
-  br: require("../../projects/whitelabel/br"),
-  en: require("../../projects/whitelabel/en"),
-  pt: require("../../projects/whitelabel/pt"),
+  br: require(`../../projects/${project}/br`),
+  en: require(`../../projects/${project}/en`),
+  pt: require(`../../projects/${project}/pt`),
 };
 
 const errorList = [];
-const logError = (key, locale) => console.error(`Missing key: "${key}" for locale: "${locale}"`);
+const logError = (key, locale) =>
+  console.error(`Missing key in ${project}: "${key}" for locale: "${locale}"`);
+const localesList = Object.keys(translations);
 
-beforeAll(() => {
-  const enTranslationKeys = Object.keys(translations.en);
-  const localesList = Object.keys(translations);
+test(`${project}: error list must be empty`, () => {
+  localesList.forEach(lang => {
+    const translationKeys = Object.keys(translations[lang]);
 
-  enTranslationKeys.forEach(key => {
-    localesList.forEach(locale => {
-      if (locale === "en") return;
-      if (!translations[locale].hasOwnProperty(key)) errorList.push([key, locale]);
+    translationKeys.forEach(key => {
+      localesList.forEach(locale => {
+        if (locale === lang) return;
+        if (!translations[locale].hasOwnProperty(key)) errorList.push([key, locale]);
+      });
     });
+
+    if (errorList.length) errorList.forEach(([key, locale]) => logError(key, locale));
+    expect(errorList.length).toBe(0);
   });
 });
 
-test("whitelabel: error list must be empty", () => {
-  if (errorList.length) errorList.forEach(([key, locale]) => logError(key, locale));
-  expect(errorList.length).toBe(0);
+test(`${project}: equal number of translations for every locale`, () => {
+  const translationsLengths = {};
+  let hasError = false;
+
+  localesList.forEach(locale => {
+    translationsLengths[locale] = Object.keys(translations[locale]).length;
+  });
+
+  const lengthsArray = Object.values(translationsLengths);
+
+  lengthsArray.forEach((item, i) => {
+    if (i > 0 && item !== lengthsArray[i - 1]) {
+      hasError = true;
+      return;
+    }
+  });
+
+  if (hasError) console.error(project, translationsLengths);
+  expect(hasError).toBe(false);
 });
