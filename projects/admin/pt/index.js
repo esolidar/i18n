cfe0f32952ebac6e20f1681927e1ec30d7530e11
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2021-07-29 15:27:06 +0100

    ENG-219 #comment Refactor: joined every admin translation into a single file

--- projects/admin/pt/index.js
+++ projects/admin/pt/index.js
@@ -1,31 +1,5 @@
-const alerts = require("./alerts.json");
-const auctions = require("./auctions.json");
-const buttons = require("./buttons.json");
-const companies = require("./companies.json");
-const countries = require("./countries.json");
-const crowdfundings = require("./crowdfundings.json");
-const forms = require("./forms.json");
-const institutions = require("./institutions.json");
-const leftMenu = require("./leftMenu.json");
-const login = require("./login.json");
 const main = require("./main.json");
-const requests = require("./requests.json");
-const tickets = require("./tickets.json");
 
-const translation = Object.assign(
-  alerts,
-  auctions,
-  buttons,
-  companies,
-  countries,
-  crowdfundings,
-  forms,
-  institutions,
-  leftMenu,
-  login,
-  main,
-  requests,
-  tickets,
-);
+const translation = Object.assign(main);
 
 module.exports = translation;
