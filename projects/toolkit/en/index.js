eeba8d0b91029c45ffcb000043722b3c678544fd
Pedro Guia <54513333+pedroguia@users.noreply.github.com>
2021-07-29 14:53:04 +0100

    ENG-212 #comment Add: added toolkit translations into its own files

--- projects/toolkit/en/index.js
+++ projects/toolkit/en/index.js
@@ -1,9 +1,5 @@
-const admin = require("../../admin/en");
-const business = require("../../business/en");
-const esolidar = require("../../esolidar/en");
-const whitelabel = require("../../whitelabel/en");
 const main = require("./main.json");
 
-const translation = Object.assign(main, admin, business, esolidar, whitelabel);
+const translation = Object.assign(main);
 
 module.exports = translation;
--- projects/toolkit/pt/index.js
+++ projects/toolkit/pt/index.js
@@ -1,9 +1,5 @@
-const admin = require("../../admin/pt");
-const business = require("../../business/pt");
-const esolidar = require("../../esolidar/pt");
-const whitelabel = require("../../whitelabel/pt");
 const main = require("./main.json");
 
-const translation = Object.assign(main, admin, business, esolidar, whitelabel);
+const translation = Object.assign(main);
 
 module.exports = translation;
