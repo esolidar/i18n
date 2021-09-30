e6e0ac541b42de8938d57cd418859f6eb71ca6c0
Joel Calheiros <joel@esolidar.com>
2021-09-30 17:08:20 +0100

    added needs texts

--- __tests__/missing.test.js
+++ __tests__/missing.test.js
@@ -1,24 +1,28 @@
 const { getAllTranslationsByProject } = require("../shared/utils/tests");
 const projectList = require("../shared/constants/projects");
 
-projectList.forEach(project => {
+projectList.forEach((project) => {
   const translations = getAllTranslationsByProject(project);
   const localesList = Object.keys(translations);
 
   test(`${project}: every key should exist in every locale`, () => {
     const errorList = [];
 
-    localesList.forEach(lang => {
+    localesList.forEach((lang) => {
       const translationKeys = Object.keys(translations[lang]);
 
-      translationKeys.forEach(key => {
-        localesList.forEach(locale => {
+      translationKeys.forEach((key) => {
+        localesList.forEach((locale) => {
           if (locale === lang) return;
-          if (!translations[locale].hasOwnProperty(key)) errorList.push([key, locale]);
+          if (!translations[locale].hasOwnProperty(key))
+            errorList.push([key, locale]);
         });
       });
 
-      if (errorList.length) errorList.forEach(([key, locale]) => logError(project, key, locale));
+      if (errorList.length)
+        errorList.forEach(([key, locale]) =>
+          console.error(project, key, locale)
+        );
       expect(errorList.length).toBe(0);
     });
   });
